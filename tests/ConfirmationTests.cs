46b77517cc20cb55bd392ea547deb391028031fd
Aaron Marks <aaron.marks@cdpehs.com>
2020-05-11 18:15:28 -0400

    Initial commit

---
+++ tests/ConfirmationTests.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace TestConsoleApp.Tests
+{
+    public class ConfirmationTests
+    {
+        [Fact]
+        public void ConfirmsAssertionTrue()
+        {
+          Assert.True(true);
+        }
+    }
+}
