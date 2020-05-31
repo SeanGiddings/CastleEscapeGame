77f8fd3f64f63c7e95f30cbd5d0a167e59c38b41
seangiddings <seandgiddings@gmail.com>
2020-05-31 15:38:37 -0400

    added empty location class

---
+++ CastleEscape/Location.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+
+namespace CastleEscape
+{
+    class Location
+    {
+            public Location()
+        {
+                
+        }
+
+    }
+}
